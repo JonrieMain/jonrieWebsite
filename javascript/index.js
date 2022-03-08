41a9d235288674962ffb562761a9b3587f2b99ec
Jonrie Morales Badiang <jonriemain@gmail.com>
2022-03-08 09:32:24 +0800

    something change and add

--- javascript/index.js
+++ javascript/index.js
@@ -1,47 +1,81 @@
 /* Scroll effect for header*/
 var header = document.querySelector('.header');
 window.addEventListener('scroll', () =>{
-header.classList.toggle('sticky', window.scrollY > 0);
+	header.classList.toggle('sticky', window.scrollY > 0);
 });
 /*Darkmode Lightmode*/
 var body = document.querySelector('.light');
 var sun = document.querySelector('#sun');
 var moon = document.querySelector('#moon');
-const menu = document.getElementById("menu").children;
+var menu = document.getElementById("menu").children;
 var jonrieLogo = document.querySelector('#jonrieLogo');
+var themeColorCount = 0;
 
+
+
+
+
+darkFunc = () =>{
+	body.style.background="black";
+	body.classList.remove('light')
+	body.classList.add('dark')
+	body.style.color="white";
+	moon.style.display="none";
+	sun.style.display="block";
+	header.style.background="black";
+	for (var i = 0; i < menu.length; i++) {
+		menu[i].style.color = "white";
+	}
+	jonrieLogo.style.color="white";
+}
+
+lightFunc = () =>{
+	body.style.background="white";
+	body.classList.remove('dark')
+	body.classList.add('light')
+	body.style.color="black";
+	moon.style.display="block"
+	sun.style.display="none"
+	header.style.background="white";
+	for (var i = 0; i < menu.length; i++) {
+		menu[i].style.color = "black";
+	}
+	jonrieLogo.style.color="black";
+
+}
+
+
+check = () =>{
+	if(localStorage.getItem("themeColor") === null){
+		lightFunc();
+	}else if(localStorage.getItem("themeColor") === "0"){
+		lightFunc();
+	}else if(localStorage.getItem("themeColor") === "1"){
+		darkFunc();
+
+	}
+}
+check();
 moon.addEventListener('click',()=>{
 	if(body.classList.contains('light')){
-		body.style.background="black";
-		body.classList.remove('light')
-		body.classList.add('dark')
-		body.style.color="white";
-		moon.style.display="none";
-		sun.style.display="block";
-		header.style.background="black";
-		for (var i = 0; i < menu.length; i++) {
-  			menu[i].style.color = "white";
-		}
-		jonrieLogo.style.color="white";
+		darkFunc();
+		themeColorCount++;
+		localStorage.setItem("themeColor", themeColorCount);
 	}
-	
 });
 
 sun.addEventListener('click',()=>{
 	if(body.classList.contains('dark')){
-		body.style.background="white";
-		body.classList.remove('dark')
-		body.classList.add('light')
-		body.style.color="black";
-		moon.style.display="block"
-		sun.style.display="none"
-		header.style.background="white";
-		for (var i = 0; i < menu.length; i++) {
-  			menu[i].style.color = "black";
+		if (themeColorCount === 0) {
+			themeColorCount = themeColorCount = 0;
+			lightFunc();
+			localStorage.setItem("themeColor", themeColorCount);
+		}else{
+			lightFunc();
+			themeColorCount--;
+			localStorage.setItem("themeColor", themeColorCount);
 		}
-		jonrieLogo.style.color="black";
 	}
-	
 });
 
-	
+
